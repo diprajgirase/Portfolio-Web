fcdeba1e52242f1aeebea134ba8f36e761b53184
Dipraj Girase <girasedipraj52@gmail.com>
2024-10-26 23:13:06 +0530

    Update Hero.js

--- components/Hero.js
+++ components/Hero.js
@@ -7,10 +7,14 @@ function Hero() {
       className="mx-auto mb-36 flex max-w-3xl flex-col items-center justify-center text-center"
     >
       <h1 className="mx-6 mb-8 flex flex-wrap items-center justify-center gap-4 text-5xl lg:text-6xl">
+      <span className="font-light tracking-tighter">Hi,I'm Dipraj</span>
+      <span className="font-light tracking-tighter">an Frontend Dev</span>
+      </h1>
+      <h2 className="mx-6 mb-8 flex flex-wrap items-center justify-center gap-4 text-5xl lg:text-6xl">
         <span className="font-light tracking-tighter">Design</span>
         <span className="font-light tracking-tighter">Develop</span>
         <span className="font-light tracking-tighter">Deploy</span>
-      </h1>
+      </h2>
       <p className="mb-10 w-[80%] text-sm text-slate-600 md:text-base">
         I design and develop web applications and deploy them in weeks not
         months. Building applications that people can use.
